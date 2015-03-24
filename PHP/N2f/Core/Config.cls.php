d1e243bde09283c0f13825db1450027954b6970d
Andrew Male <amale@zibings.com>
2015-03-24 12:33:21 -0400

    [PHP] Documenting Config and LoggerConfig classes

--- PHP/N2f/Core/Config.cls.php
+++ PHP/N2f/Core/Config.cls.php
@@ -14,13 +14,49 @@
 	 * @package N2F
 	 */
 	class Config {
+		/**
+		 * The current charset for this configuration.
+		 * 
+		 * @var string
+		 */
 		public $Charset;
+		/**
+		 * The current logger config for this configuration.
+		 * 
+		 * @var \N2f\LoggerConfig
+		 */
 		public $Logger;
+		/**
+		 * The current collection of extensions for this configuration.
+		 * 
+		 * @var array
+		 */
 		public $Extensions;
+		/**
+		 * The current hash for this configuration.
+		 * 
+		 * @var string
+		 */
 		public $Hash;
+		/**
+		 * The current locale for this configuration.
+		 * 
+		 * @var string
+		 */
 		public $Locale;
+		/**
+		 * The current timezone for this configuration.
+		 * 
+		 * @var string
+		 */
 		public $Timezone;
 
+		/**
+		 * Creates a new Config instance.
+		 * 
+		 * @param array $config Optional array of configuration values.
+		 * @return void
+		 */
 		public function __construct(array $config = null) {
 			if ($config === null || count($config) < 1) {
 				$this->Charset = 'utf8';
@@ -54,9 +90,25 @@
 	 * @package N2F
 	 */
 	class LoggerConfig {
+		/**
+		 * Whether or not logs should be dumped.
+		 * 
+		 * @var bool
+		 */
 		public $DumpLogs;
+		/**
+		 * The current log level for this instance.
+		 * 
+		 * @var int
+		 */
 		public $LogLevel;
 
+		/**
+		 * Creates a new LoggerConfig instance.
+		 * 
+		 * @param array $config Optional array of configuration values.
+		 * @return void
+		 */
 		public function __construct(array $config = null) {
 			if ($config === null || count($config) < 1) {
 				$this->DumpLogs = false;
@@ -66,25 +118,7 @@
 				$this->LogLevel = (array_key_exists('log_level', $config)) ? $config['log_level'] : N2F_LOG_ERROR;
 			}
 
-			return $this;
-		}
-
-		public function SetDumpLogs($Value) {
-			if (is_bool($Value)) {
-				$this->DumpLogs = $Value;
-			}
-
-			return $this;
-		}
-
-		public function SetLogLevel($Level) {
-			if (!Logger::ValidLevel($Level)) {
-				return $this;
-			}
-
-			$this->LogLevel = $Level;
-
-			return $this;
+			return;
 		}
 	}
 
