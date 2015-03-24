1680a5004abd8474560c1c76b17b29eca264d792
Andrew Male <amale@zibings.com>
2015-03-24 13:52:34 -0400

    [PHP] Documenting Extension class

--- PHP/N2f/Core/Extension.cls.php
+++ PHP/N2f/Core/Extension.cls.php
@@ -14,12 +14,45 @@
 	 * @package N2F
 	 */
 	class Extension {
+		/**
+		 * The name of the extension.
+		 * 
+		 * @var string
+		 */
 		private $_Name;
+		/**
+		 * The author's name of the extension.
+		 * 
+		 * @var string
+		 */
 		private $_Author;
+		/**
+		 * The version of the extension.
+		 * 
+		 * @var string
+		 */
 		private $_Version;
+		/**
+		 * The base file of the extension.
+		 * 
+		 * @var string
+		 */
 		private $_BaseFile;
+		/**
+		 * Array of automatically included files.
+		 * 
+		 * @var array
+		 */
 		private $_AutoIncludes;
 
+		/**
+		 * Creates a new Extension instance.
+		 * 
+		 * @param string $Name Extension name value.
+		 * @param string $Author Extension author value.
+		 * @param string $Version Extension version value.
+		 * @return void
+		 */
 		public function __construct($Name, $Author, $Version) {
 			$this->_Name = $Name;
 			$this->_Author = $Author;
@@ -30,32 +63,69 @@
 			return;
 		}
 
+		/**
+		 * Retrieves the extension name.
+		 * 
+		 * @return string Extension name value.
+		 */
 		public function GetName() {
 			return $this->_Name;
 		}
 
+		/**
+		 * Retrieves the extension author.
+		 * 
+		 * @return string Extension author value.
+		 */
 		public function GetAuthor() {
 			return $this->_Author;
 		}
 
+		/**
+		 * Retreives the extension version.
+		 * 
+		 * @return string Extension version value.
+		 */
 		public function GetVersion() {
 			return $this->_Version;
 		}
 
+		/**
+		 * Retrieves the extension base file.
+		 * 
+		 * @return string Extension base file value.
+		 */
 		public function GetBaseFile() {
 			return $this->_BaseFile;
 		}
 
+		/**
+		 * Retrieves the extension auto includes.
+		 * 
+		 * @return array Extension auto include array.
+		 */
 		public function GetAutoIncludes() {
 			return $this->_AutoIncludes;
 		}
 
+		/**
+		 * Sets the extension base file.
+		 * 
+		 * @param string $BaseFile String value for extension base file.
+		 * @return void
+		 */
 		public function SetBaseFile($BaseFile) {
 			$this->_BaseFile = $BaseFile;
 
 			return;
 		}
 
+		/**
+		 * Sets the extension auto includes.
+		 * 
+		 * @param array $AutoIncludes Array of auto includes.
+		 * @return void
+		 */
 		public function SetAutoIncludes(array $AutoIncludes) {
 			if (count($AutoIncludes) < 1) {
 				return;
