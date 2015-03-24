8cda4e4dc46ae530e6b510d7e4ed5a4aa94ef91d
Andrew Male <amale@zibings.com>
2015-03-24 15:20:37 -0400

    [PHP] Documenting SampleExtension classes

--- PHP/N2f/Extensions/SampleExtension/SampleExtension.ext.php
+++ PHP/N2f/Extensions/SampleExtension/SampleExtension.ext.php
@@ -1,20 +1,47 @@
 <?php
 
+	/**
+	 * Class to receive initialization from N2f core.
+	 */
 	class SampleExtension extends N2f\ExtensionBase {
+		/**
+		 * Initializes the SampleExtension extension via
+		 * a N2f system call.
+		 * 
+		 * @param \N2f\N2f $N2f The N2f instance initializing the extension.
+		 * @return void
+		 */
 		public function Initialize(N2f\N2f &$N2f) {
 			$N2f->LinkExecuteNode(new SampleNode);
 			$N2f->LinkConfigNode(new SampleConfigNode);
 			$N2f->GetLogger()->LinkOutputNode(new LogDump);
+
+			return;
 		}
 	}
 
+	/**
+	 * Node to catch CLI configuration dispatches.
+	 */
 	class SampleConfigNode extends N2f\NodeBase {
+		/**
+		 * Creates a new SampleConfigNode instance.
+		 * 
+		 * @return void
+		 */
 		public function __construct() {
 			$this->SetKey('SampleConfigNode')->SetVersion('0.1');
 
 			return;
 		}
 
+		/**
+		 * Processes a ConfigDispatch dispatch.
+		 * 
+		 * @param \N2f\N2f $Sender The executing N2f instance.
+		 * @param \N2f\DispatchBase $Dispatch The DispatchBase instance for the chain, must be a ConfigDispatch.
+		 * @return void
+		 */
 		public function Process($Sender, N2f\DispatchBase &$Dispatch) {
 			if ($Dispatch === null || !($Sender instanceof N2f\N2f)) {
 				return;
@@ -39,13 +66,28 @@
 		}
 	}
 
+	/**
+	 * A sample node to demonstrate some functionality.
+	 */
 	class SampleNode extends N2f\NodeBase {
+		/**
+		 * Creates a new SampleNode instance.
+		 * 
+		 * @return void
+		 */
 		public function __construct() {
 			$this->SetKey('SampleNode')->SetVersion('0.1');
 
 			return;
 		}
 
+		/**
+		 * Processes a DispatchBase dispatch.
+		 * 
+		 * @param \N2f\N2f $Sender The executing N2f instance.
+		 * @param \N2f\DispatchBase $Dispatch The DispatchBase instance for the chain.
+		 * @return void
+		 */
 		public function Process($Sender, N2f\DispatchBase &$Dispatch) {
 			if ($Dispatch === null || !($Sender instanceof N2f\N2f)) {
 				return;
@@ -87,13 +129,28 @@
 		}
 	}
 
+	/**
+	 * A node to handle log dumps from the core.
+	 */
 	class LogDump extends N2f\NodeBase {
+		/**
+		 * Creates a new LogDump instance.
+		 * 
+		 * @return void
+		 */
 		public function __construct() {
 			$this->SetKey('LogDump')->SetVersion('0.1');
 
 			return;
 		}
 
+		/**
+		 * Processes a LogOutputDispatch dispatch.
+		 * 
+		 * @param mixed $Sender The sender value.
+		 * @param \N2f\DispatchBase $Dispatch The DispatchBase instance for the chain, must be a LogOutputDispatch.
+		 * @return void
+		 */
 		public function Process($Sender, N2f\DispatchBase &$Dispatch) {
 			if ($Dispatch === null || !($Dispatch instanceof N2f\LogOutputDispatch)) {
 				return;
