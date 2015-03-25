d336c6c1a5652b91e56fab6e4d9c3548eeeb7d00
Andrew Male <amale@zibings.com>
2015-03-25 00:36:10 -0400

    [C#] Adding code-docs for DispatchBase class, because I will never do all code-docs at the end again

--- CSharp/Bases/DispatchBase.cs
+++ CSharp/Bases/DispatchBase.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace N2F
 {
+	/// <summary>
+	/// Abstract base for dispatches.
+	/// </summary>
 	public abstract class DispatchBase
 	{
 		protected List<object> _Results;
@@ -11,12 +14,31 @@ namespace N2F
 		protected bool _IsConsumed;
 		protected bool _IsValid;
 
+		/// <summary>
+		/// Collection of results for the dispatch.
+		/// </summary>
 		public List<object> Results { get { return this._Results; } }
+		/// <summary>
+		/// Whether or not the dispatch is consumable.
+		/// </summary>
 		public bool IsConsumable { get { return this._IsConsumable; } }
+		/// <summary>
+		/// Whether or not the dispatch is stateful (can have multiple results).
+		/// </summary>
 		public bool IsStateful { get { return this._IsStateful; } }
+		/// <summary>
+		/// Whether or not the dispatch is consumed.
+		/// </summary>
 		public bool IsConsumed { get { return this._IsConsumed; } }
+		/// <summary>
+		/// Whether or not the dispatch is valid.
+		/// </summary>
 		public bool IsValid { get { return this._IsValid; } }
 
+		/// <summary>
+		/// Marks the dispatch as consumed if it is consumable (and not consumed).
+		/// </summary>
+		/// <returns>Whether or not the consume action was successful.</returns>
 		public bool Consume()
 		{
 			if (this._IsConsumable && !this._IsConsumed)
@@ -29,13 +51,24 @@ namespace N2F
 			return false;
 		}
 
+		/// <summary>
+		/// Returns the collection of results (if any) from the dispatch.
+		/// </summary>
+		/// <returns>Any results assigned to the dispatch.</returns>
 		public virtual List<object> GetResults()
 		{
 			return new List<object>(this._Results);
 		}
 
+		/// <summary>
+		/// Method to initialize the dispatch for processing.
+		/// </summary>
+		/// <param name="Input">Input information to initialize the dispatch.</param>
 		public abstract void Initialize(object Input);
 
+		/// <summary>
+		/// Marks the dispatch as consumable.
+		/// </summary>
 		protected void MakeConsumable()
 		{
 			this._IsConsumable = true;
@@ -43,6 +76,9 @@ namespace N2F
 			return;
 		}
 
+		/// <summary>
+		/// Marks the dispatch as stateful (able to have multiple results).
+		/// </summary>
 		protected void MakeStateful()
 		{
 			this._IsStateful = true;
@@ -50,6 +86,9 @@ namespace N2F
 			return;
 		}
 
+		/// <summary>
+		/// Marks the dispatch as valid (ready for processing).
+		/// </summary>
 		protected void MakeValid()
 		{
 			this._IsValid = true;
@@ -57,6 +96,11 @@ namespace N2F
 			return;
 		}
 
+		/// <summary>
+		/// Sets a result for the dispatch, either adding it to the collection if the dispatch is stateful, or adding it as the only result if not stateful.
+		/// </summary>
+		/// <param name="Result">Object to set as result.</param>
+		/// <returns>The current <see cref="DispatchBase"/> instance.</returns>
 		public DispatchBase SetResult(object Result)
 		{
 			if (!this._IsStateful)
