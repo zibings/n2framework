003edd89a79a3371c47766f20a45bb2134c20757
Andrew Male <amale@zibings.com>
2015-03-25 07:10:40 -0400

    [C#] Adding the ExtensionBase abstract class

---
+++ CSharp/Bases/ExtensionBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace N2F
+{
+	/// <summary>
+	/// Abstract base for Extension receivers.
+	/// </summary>
+	public abstract class ExtensionBase
+	{
+		/// <summary>
+		/// Method to initialize an extension.
+		/// </summary>
+		/// <param name="N2f">The current executing instance.</param>
+		abstract public void Initialize(N2f.N2f N2f);
+	}
+}
