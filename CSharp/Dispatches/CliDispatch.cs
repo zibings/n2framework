84585a3c7545068e12e0d6e8e8eb2cda15bb77ff
Andrew Male <amale@zibings.com>
2015-03-26 11:07:59 -0400

    [C#] Getting started on CliDispatch class

---
+++ CSharp/Dispatches/CliDispatch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace N2f
+{
+	public class CliDispatch : DispatchBase
+	{
+		private bool _IsWindows;
+
+		public bool IsWindows { get { return this._IsWindows; } }
+
+		public CliDispatch()
+		{
+			switch (Environment.OSVersion.Platform)
+			{
+				case PlatformID.MacOSX:
+				case PlatformID.Unix:
+					this._IsWindows = false;
+
+					break;
+				default:
+					this._IsWindows = true;
+
+					break;
+			}
+
+			return;
+		}
+
+		public override void Initialize(object Input)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
