feae8466edf516534b43b8c7caca4a31401c0d05
Andrew Male <amale@zibings.com>
2015-04-26 20:29:35 -0400

    [C#] Adding a structures file because I think that's where I'll make shtuff

---
+++ CSharp/Structures.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace N2f
+{
+	/// <summary>
+	/// A command line argument that has been parsed.
+	/// </summary>
+	public struct ParsedArgument
+	{
+		/// <summary>
+		/// The key of the argument.
+		/// </summary>
+		public string Key { get; set; }
+		/// <summary>
+		/// The lowercase version of the key.
+		/// </summary>
+		public string LoweredKey { get; set; }
+		/// <summary>
+		/// The original string that was parsed.
+		/// </summary>
+		public string Original { get; set; }
+		/// <summary>
+		/// The value of the argument.
+		/// </summary>
+		public string Value { get; set; }
+	}
+}
