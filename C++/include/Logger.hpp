21e9a2749ff5fed7b0d92a6c8f89c7e23683f16d
Andrew Male <amale@zibings.com>
2015-07-17 22:02:28 -0400

    [C++] Adding a bit of work I'd left over for the Logger class

--- C++/include/Logger.hpp
+++ C++/include/Logger.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <BaseClasses/NodeBase.hpp>
+#include <Config.hpp>
+#include <Helpers/ChainHelper.hpp>
 #include <Logger.hpp>
 #include <map>
 #include <vector>
@@ -9,6 +11,8 @@ namespace N2f
 {
 	class Logger
 	{
-
+	protected:
+		ChainHelper _logChain, _outputChain;
+		LoggerConfig _config;
 	};
 }
