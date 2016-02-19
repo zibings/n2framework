f6cc6ed1ca1d0eda3d47f9551a81c9e413f89b7c
Andrew Male <amale@zibings.com>
2016-02-19 10:27:05 -0500

    [C++] Adding a pragma to deal with Windows demanding use of strcpy_s() instead of strcpy() in NodeBase

--- C++/src/BaseClasses/NodeBase.cpp
+++ C++/src/BaseClasses/NodeBase.cpp
@@ -7,7 +7,11 @@ namespace N2f
 		// TODO: Add logging here in the future, for now we're just looking at basic usage
 		if ((this->_key == NULL || strlen(this->_key) < 1) && Key != NULL && strlen(Key) > 0)
 		{
+#if defined(_WIN32)
+			strcpy_s(this->_key, sizeof(this->_version), Key);
+#else
 			strcpy(this->_key, Key);
+#endif
 		}
 
 		return;
@@ -18,7 +22,11 @@ namespace N2f
 		// TODO: Add logging here in the future, for now we're just looking at basic usage
 		if ((this->_version == NULL || strlen(this->_version) < 1) && Version != NULL && strlen(Version) > 0)
 		{
+#if defined(_WIN32)
+			strcpy_s(this->_version, sizeof(this->_version), Version);
+#else
 			strcpy(this->_version, Version);
+#endif
 		}
 
 		return;
