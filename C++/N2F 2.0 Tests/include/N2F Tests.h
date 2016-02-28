bcd3311aabbde6f91cdbe78e22424c2e5c0e4e6d
Andrew Male <amale@zibings.com>
2016-02-28 00:50:59 -0500

    [C++] Adding UnitTest++ so we can start verifying the framework

---
+++ C++/N2F 2.0 Tests/include/N2F Tests.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include <UnitTest++/UnitTest++.h>
+#include <N2f.hpp>
---
+++ C++/N2F 2.0 Tests/src/N2F Tests.cpp
@@ -0,0 +1,6 @@
+#include <N2F Tests.h>
+
+int main(int argc, char *argv[])
+{
+	return UnitTest::RunAllTests();
+}
