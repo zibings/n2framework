0b20c5d0728ba7d0f202e8db06d418433a624e36
Andrew Male <amale@zibings.com>
2016-02-28 20:39:13 -0500

    [PHP] Undoing test failure of N2fFunctionTests::testIsWindowsOrNot()

--- PHP/Tests/FunctionTest.php
+++ PHP/Tests/FunctionTest.php
@@ -2,7 +2,7 @@
 
 	class N2fFunctionTests extends PHPUnit_Framework_TestCase {
 		public function testIsWindowsOrNot() {
-			$this->assertEquals(strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN', env_is_windows());
+			$this->assertEquals(strtoupper(substr(PHP_OS, 0, 3)) === 'WIN', env_is_windows());
 
 			return;
 		}
