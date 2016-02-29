e791a623945feb2effc714d29a9009328bf4da54
Andrew Male <amale@zibings.com>
2016-02-28 21:08:08 -0500

    [PHP] Adding more tests for functions (quasi-broken assert for testParamsAssignedValues())

--- PHP/Tests/FunctionTest.php
+++ PHP/Tests/FunctionTest.php
@@ -6,4 +6,75 @@
 
 			return;
 		}
+
+		public function testParamsReturnsCorrectNumberOfArgs() {
+			$arguments = array(
+				'test',
+				'test2',
+				'test3'
+			);
+
+			$args = env_parse_params($arguments);
+
+			$this->assertEquals(3, count($args));
+
+			return;
+		}
+
+		public function testParamsReturnsInsensitiveKeys() {
+			$arguments = array(
+				'teSt',
+				'Test2',
+				'TEST3'
+			);
+
+			$args = env_parse_params($arguments, true);
+			$index = 0;
+
+			foreach ($args as $key => $val) {
+				$this->assertNotEquals($key, $arguments[$index]);
+
+				++$index;
+			}
+
+			return;
+		}
+
+		public function testParamsReturnSensitiveKeys() {
+			$arguments = array(
+				'teSt',
+				'Test2',
+				'TEST3'
+			);
+
+			$args = env_parse_params($arguments);
+			$index = 0;
+
+			foreach ($args as $key => $val) {
+				$this->assertEquals($key, $arguments[$index]);
+
+				++$index;
+			}
+
+			return;
+		}
+
+		public function testParamsAssignedValues() {
+			$arguments = array(
+				'test=value',
+				'test2="howdy mom!"',
+				'-test3',
+				'value3'
+			);
+
+			$args = env_parse_params($arguments);
+
+			$this->assertEquals(3, count($args));
+
+			$this->assertEquals('value', $args['test']);
+			$this->assertEquals('howdy mom!', $args['test2']);
+			$this->assertEquals('test3', $args['test3']);
+
+			return;
+		}
 	}
