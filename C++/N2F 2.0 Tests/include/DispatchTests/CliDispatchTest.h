df4ace62d6c506b1ceed3a9c3ba82e4bebc8213d
Andrew Male <amale@zibings.com>
2016-03-02 11:29:52 -0500

    [C++] Added tests for CliDispatch after moving CliDispatch into the N2f namespace

--- C++/N2F 2.0 Tests/include/DispatchTests/CliDispatchTest.h
+++ C++/N2F 2.0 Tests/include/DispatchTests/CliDispatchTest.h
@@ -1,1 +1,110 @@
 #pragma once
+
+#include <UnitTest++/UnitTest++.h>
+#include <N2f.hpp>
+
+SUITE(CliDispatchTest)
+{
+	TEST(CliDispatchProducesCorrectCount)
+	{
+		N2f::CliDispatch disp;
+		disp.Initialize(2, new char*[2] { "test", "two" });
+
+		CHECK_EQUAL(2, disp.NumResults());
+	}
+
+	TEST(CliDispatchProducesCorrectRawString)
+	{
+		N2f::CliDispatch disp;
+		disp.Initialize(2, new char*[2] { "test", "two" });
+
+		CHECK_EQUAL("test two", disp.GetParameterString());
+	}
+
+	TEST(CliDispatchProducesCorrectRawParameters)
+	{
+		N2f::CliDispatch disp;
+		disp.Initialize(2, new char*[2]{ "test", "two" });
+
+		auto params = disp.GetRawParameters();
+
+		CHECK_EQUAL(2, params.size());
+		CHECK_EQUAL("test", params[0]);
+		CHECK_EQUAL("two", params[1]);
+	}
+
+	TEST(CliDispatchProducesCorrectVariantMapCount)
+	{
+		N2f::CliDispatch disp;
+		disp.Initialize(5, new char*[5] { "teSt", "-test2", "value", "test3=hey dude", "tEst" });
+
+		auto paramMap = disp.GetParameterMap(false);
+
+		CHECK_EQUAL(4, paramMap.size());
+	}
+
+	TEST(CliDispatchProduceCorreVariantMapValues)
+	{
+		N2f::CliDispatch disp;
+		disp.Initialize(5, new char*[5] { "teSt", "-test2", "value", "test3=hey dude", "tEst" });
+
+		auto paramMap = disp.GetParameterMap(false);
+
+		auto expectedMap = std::map<std::string, std::string>();
+		expectedMap.insert(std::pair<std::string, std::string>("teSt", "true"));
+		expectedMap.insert(std::pair<std::string, std::string>("test2", "value"));
+		expectedMap.insert(std::pair<std::string, std::string>("test3", "hey dude"));
+		expectedMap.insert(std::pair<std::string, std::string>("tEst", "true"));
+
+		REQUIRE CHECK_EQUAL(expectedMap.size(), paramMap.size());
+
+		auto pmIter = paramMap.begin();
+		auto emIter = expectedMap.begin();
+
+		for (; pmIter != paramMap.end(); )
+		{
+			CHECK_EQUAL(emIter->first, pmIter->first);
+			CHECK_EQUAL(emIter->second, pmIter->second);
+
+			++pmIter;
+			++emIter;
+		}
+	}
+
+	TEST(CliDispatchProducesCorrectInvariantMapCount)
+	{
+		N2f::CliDispatch disp;
+		disp.Initialize(5, new char*[5]{ "teSt", "-test2", "value", "test3=hey dude", "tEst" });
+
+		auto paramMap = disp.GetParameterMap(true);
+
+		CHECK_EQUAL(3, paramMap.size());
+	}
+
+	TEST(CliDispatchProducesCorrectInvariantMapValues)
+	{
+		N2f::CliDispatch disp;
+		disp.Initialize(5, new char*[5]{ "teSt", "-test2", "value", "test3=hey dude", "tEst" });
+
+		auto paramMap = disp.GetParameterMap(true);
+
+		auto expectedMap = std::map<std::string, std::string>();
+		expectedMap.insert(std::pair<std::string, std::string>("test", "true"));
+		expectedMap.insert(std::pair<std::string, std::string>("test2", "value"));
+		expectedMap.insert(std::pair<std::string, std::string>("test3", "hey dude"));
+
+		REQUIRE CHECK_EQUAL(expectedMap.size(), paramMap.size());
+
+		auto pmIter = paramMap.begin();
+		auto emIter = expectedMap.begin();
+
+		for (; pmIter != paramMap.end(); )
+		{
+			CHECK_EQUAL(emIter->first, pmIter->first);
+			CHECK_EQUAL(emIter->second, pmIter->second);
+
+			++pmIter;
+			++emIter;
+		}
+	}
+}
--- C++/include/Dispatches/CliDispatch.hpp
+++ C++/include/Dispatches/CliDispatch.hpp
@@ -7,32 +7,35 @@
 #include <string>
 #include <vector>
 
-class CliDispatch : public N2f::DispatchBase
+namespace N2f
 {
-public:
-	CliDispatch();
-	~CliDispatch();
+	class CliDispatch : public N2f::DispatchBase
+	{
+	public:
+		CliDispatch();
+		~CliDispatch();
 
-	/* N2f::DispatchBase methods */
+		/* N2f::DispatchBase methods */
 
-	virtual void Initialize();
-	virtual int NumResults();
-	virtual void SetResult();
+		virtual void Initialize();
+		virtual int NumResults();
+		virtual void SetResult();
 
-	/* Now our versions */
+		/* Now our versions */
 
-	CliDispatch &Initialize(int argc, char *argv[]);
-	const bool IsWindows();
-	const std::string GetParameterString();
-	const std::vector<std::string> GetRawParameters();
-	const std::map<std::string, std::string> GetParameterMap(bool invariantKey);
+		CliDispatch &Initialize(int argc, char *argv[]);
+		const bool IsWindows();
+		const std::string GetParameterString();
+		const std::vector<std::string> GetRawParameters();
+		const std::map<std::string, std::string> GetParameterMap(bool invariantKey);
 
-protected:
-	typedef std::pair<std::string, std::string> MAP_PAIR;
+	protected:
+		typedef std::pair<std::string, std::string> MAP_PAIR;
 
-	std::map<std::string, std::string> mappedParams, mappedInvariantParams;
-	std::vector<std::string> rawParams;
-	std::string raw;
+		std::map<std::string, std::string> mappedParams, mappedInvariantParams;
+		std::vector<std::string> rawParams;
+		std::string raw;
 
-	void insertMappedPair(std::string key, std::string val);
-};
+		void insertMappedPair(std::string key, std::string val);
+	};
+}
