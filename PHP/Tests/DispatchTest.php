36a7708b379812f5d018c7b0bf2c41b843cee957
Andrew Male <amale@zibings.com>
2016-03-04 07:07:59 -0500

    [PHP] First Dispatch test

--- PHP/Tests/DispatchTest.php
+++ PHP/Tests/DispatchTest.php
@@ -1,6 +1,10 @@
 <?php
 
 	class DispatchTest extends PHPUnit_Framework_TestCase {
-		public function testCliDispatch_hasCorrectArgCount() {
+		public function testConfigDispatch_getsExtension() {
+			$disp = new N2f\ConfigDispatch();
+			$disp->Initialize(array('ext=Test'));
+
+			$this->assertEquals('Test', $disp->GetExt());
 		}
 	}
