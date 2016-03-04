dd4fa56bff0a496b197876cbedda71f327f85cf1
Andrew Male <amale@zibings.com>
2016-03-03 21:25:18 -0500

    [PHP] Adding base node tests

--- PHP/Tests/BaseTest.php
+++ PHP/Tests/BaseTest.php
@@ -10,6 +10,20 @@
 		}
 	}
 
+	class InvalidNode extends N2f\NodeBase {
+		public function __construct() { }
+
+		public function Process($Sender, N2f\DispatchBase &$Dispatch) { }
+	}
+
+	class ValidNode extends N2f\NodeBase {
+		public function __construct() {
+			$this->SetKey("ValidNode")->SetVersion("1");
+		}
+
+		public function Process($Sender, N2f\DispatchBase &$Dispatch) { }
+	}
+
 	class N2fBaseTests extends PHPUnit_Framework_TestCase {
 		public function testDispatchIsNotValid() {
 			$disp = new InvalidDispatch();
@@ -23,5 +37,17 @@
 			$disp->Initialize(null);
 
 			$this->assertEquals(true, $disp->IsValid());
+		}
+
+		public function testNodeIsNotValid() {
+			$node = new InvalidNode();
+
+			$this->assertEquals(false, $node->IsValid());
+		}
+
+		public function testNodeIsValid() {
+			$node = new ValidNode();
+
+			$this->assertEquals(true, $node->IsValid());
 		}
 	}
